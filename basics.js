93c929c9cbb32668a5376c79becd1e13f10a1fe5
Can Güzel <canguzel5959@gmail.com>
2024-12-28 14:06:06 +0300

    removed all old lines

--- basics.js
+++ basics.js
@@ -2,21 +2,10 @@ const userName = "Can"
 const userSurname = "Güzel"
 const userAge = 16
 const isAdult = Math.round(userAge.toFixed(2)) >= 18
-console.log(`Wellcome ${userName}, your age is ${userAge}.`)
-
-// const pricePerDroid = 800;
-// const orderedQuantity = 6;
-// const deliveryFee = 50;
-// const totalPrice = pricePerDroid*orderedQuantity+deliveryFee;
-// const message = `You ordered droids worth ${totalPrice} credits`;
-// console.log(message)
-
-console.warn(`Hoşgeldin ${userName[0] + userSurname[0]}, Uzunluk : ${message.length} karakter.`);
+console.log(`Wellcome ${userName[0] + userSurname[0]}, your age is ${userAge}.`)
 
 if (Boolean(isAdult) != true) {
         console.error(`User ${userName + " " + userSurname} is not adult.`)
     } else {
         console.info(`User ${userName + " " + userSurname} is adult.`)
     }
-
-// console.log(Number.parseFloat("12.46qwe79"));
