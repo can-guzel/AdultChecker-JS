3a6f5699d2056a916ce9e93dde306a3028cd82bf
can-guzel <canguzel5959@gmail.com>
2024-12-28 13:57:48 +0300

    changed texts to English and closed unnecesarry codeblocks

--- basics.js
+++ basics.js
@@ -2,21 +2,21 @@ const userName = "Can"
 const userSurname = "Güzel"
 const userAge = 16
 const isAdult = Math.round(userAge.toFixed(2)) >= 18
-console.log(`Hoşgeldin ${userName}, yaşın ${userAge}.`)
+console.log(`Wellcome ${userName}, your age is ${userAge}.`)
 
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFee = 50;
-const totalPrice = pricePerDroid*orderedQuantity+deliveryFee;
-const message = `You ordered droids worth ${totalPrice} credits`;
-console.log(message)
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid*orderedQuantity+deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits`;
+// console.log(message)
 
 console.warn(`Hoşgeldin ${userName[0] + userSurname[0]}, Uzunluk : ${message.length} karakter.`);
 
 if (Boolean(isAdult) != true) {
-        console.error(`Kullanıcı ${userName + " " + userSurname} reşit değil`)
+        console.error(`User ${userName + " " + userSurname} is not adult.`)
     } else {
-        console.info(`User ${userName + " " + userSurname} reşit.`)
+        console.info(`User ${userName + " " + userSurname} is adult.`)
     }
 
-console.log(Number.parseFloat("12.46qwe79"));
+// console.log(Number.parseFloat("12.46qwe79"));
